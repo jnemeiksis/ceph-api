47309ca8d2fcfd6f8bbd5cacf0c097b9aec266b4
Jonas Nemeiksis <jonas.nemeiksis@hostinger.com>
2021-06-15 12:21:39 +0300

    init

--- api/api.go
+++ api/api.go
@@ -97,7 +97,7 @@ func GetBucketStatsJSON(endpoint string, bucket string) string {
 
 // GetUserStatsJSON return user stats in json
 func GetUserStatsJSON(endpoint string, user string) string {
-	url := endpoint + "/admin/user?user=" + user
+	url := endpoint + "/admin/user?uid=" + user
 	ustats := adminAPI(url)
 	return ustats
 }
@@ -112,11 +112,11 @@ func ListUsers(endpoint string) string {
 }
 
 // GetUserBuckets return bucket stats
-func GetUserBuckets(endpoint string, user string) string {
-	url := endpoint + "/admin/bucket?uid=" + user
-	ubuckets := adminAPI(url)
-	return ubuckets
-}
+//func GetUserBuckets(endpoint string, user string) string {
+//	url := endpoint + "/admin/bucket?uid=" + user
+//	ubuckets := adminAPI(url)
+//	return ubuckets
+//}
 
 // GetUserQuotasJSON return user quotas
 func GetUserQuotasJSON(endpoint string, user string) string {
